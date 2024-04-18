f5cdc0d5e4f9d510f4e7532f3da054b4f88451b7
Jeya Surya Shankar B <111894416+Jsuryaboi-08@users.noreply.github.com>
2024-04-18 22:34:45 +0530

    Update nooploop.ino

--- nooploop.ino
+++ nooploop.ino
@@ -1,12 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial1.begin(921600);
+  Serial.begin(921600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   char buf[128]={0x54,0,1};
   buf[127]=0x55;
-  Serial1.write(buf,128);  
+  Serial.write(buf,128);  
   delay(5000);
 }
